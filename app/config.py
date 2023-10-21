d25a59ac3d254b086d41fe86da4e8210739bcc4b
Noha <nohashehab.iti@gmail.com>
2023-10-21 10:15:41 +0300

    creating config file

---
+++ app/config.py
@@ -0,0 +1,22 @@
+class Config:
+    @staticmethod
+    def init_app():
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG=True
+    SQLALCHEMY_DATABASE_URI= "sqlite:///project.sqlite"
+
+
+class ProductionConfig(Config):
+    DEBUG=False
+    # postgresql://username:password@localhost:portnumber/dbname
+    SQLALCHEMY_DATABASE_URI= "postgresql://flask:iti@localhost:5432/flask_mvt_pymans1"
+
+
+
+projectConfig={
+    "dev": DevelopmentConfig,
+    'prd': ProductionConfig
+}
